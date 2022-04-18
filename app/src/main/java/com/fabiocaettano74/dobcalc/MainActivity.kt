651dea911e171657d55a2fadc5183ebf9e5f4752
fabiocaettano <fabio.caettano74@gmail.com>
2022-04-17 21:49:38 -0300

    lesson 88

--- app/src/main/java/com/fabiocaettano74/dobcalc/MainActivity.kt
+++ app/src/main/java/com/fabiocaettano74/dobcalc/MainActivity.kt
@@ -4,16 +4,20 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
 
         btnDatePicker.setOnClickListener{
             clickDatePicker()
@@ -23,11 +27,13 @@ class MainActivity : AppCompatActivity() {
     fun clickDatePicker(){
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
-        val month = myCalendar.get(Calendar.MONTH)
+        val month = myCalendar.get(Calendar.MONTH
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view,year,month,dayOfMonth ->
-            Toast.makeText(this,"btnDatePicker pressed", Toast.LENGTH_LONG).show()
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view,selectedYear,selectedMonth,selectedDayOfMonth ->
+            //Toast.makeText(this,"btnDatePicker pressed", Toast.LENGTH_LONG).show()
+            val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+            tvSelectedDate?.text = selectedDate
         },
         year,
         month,
